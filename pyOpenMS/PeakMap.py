d42a6206650f33554a54190ec003396c04c9a649
uweschmitt <uschmitt@mineway.de>
2011-07-21 19:47:15 +0200

    PeakMap.py nachgeliefert

--- pyOpenMS/PeakMap.py
+++ pyOpenMS/PeakMap.py
@@ -3,3 +3,10 @@ class PeakMap(object):
 
     def __init__(self, specs = None):
         self.specs = specs
+
+    def __len__(self):
+        return len(self.specs)
+
+
+    def __iter__(self):
+        return iter(self.specs)
