a2ecf55307afd246ed871747c065cae8a7ce5091
uweschmitt <uschmitt@mineway.de>
2011-07-27 13:26:55 +0200

    script extractScans.py implementiert

---
+++ pyOpenMS/extractScans.py
@@ -0,0 +1,24 @@
+from pyOpenMS import *
+import sys
+
+fileNameIn, fileNameOut = sys.argv[1:3]
+scanMinIdx, scanMaxIdx = map(int, sys.argv[3:5])
+
+assert scanMinIdx <= scanMaxIdx
+
+print
+print "load file"
+peakMap = loadMzXMLFile(fileNameIn)
+
+print "file loaded"
+
+# scanMaxIdx is inclusive !
+peakMap.specs = peakMap.specs[scanMinIdx:scanMaxIdx+1]
+
+    
+saveMzXMLFile(peakMap, fileNameOut)   
+print "file saved"
+
+    
+    
+
