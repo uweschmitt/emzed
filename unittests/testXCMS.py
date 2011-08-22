ba142a2534bfa19c5d500c27fb6f431c5ec0d1d0
uweschmitt <uschmitt@mineway.de>
2011-08-22 18:15:25 +0200

    expanded unittest for Table.py

--- unittests/testXCMS.py
+++ unittests/testXCMS.py
@@ -5,5 +5,9 @@ def testFeatureDetector():
     ds = loadMzXmlFile("data/short_100scans.mzXml")
     table = XCMSPeakDetector(ds)
     assert len(table) == 44
+    assert len(table.colNames) ==  13
+    assert len(table.colTypes) ==  13
+    assert set(table.colNames) == set(XCMSFeatureParser.headlines[0].split() )
+    
     
 
