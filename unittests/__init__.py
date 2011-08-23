da7b41e2e2dc24c58bc50875bb731771c97bfad6
uweschmitt <uschmitt@mineway.de>
2011-08-23 14:20:16 +0200

    fixed logging issue in unittests

--- unittests/__init__.py
+++ unittests/__init__.py
@@ -7,6 +7,9 @@ def setup():
     shutil.rmtree("temp_output", ignore_errors=True)
     os.mkdir("temp_output")
 
+    import config_logger
+    config_logger.do_config()
+
 def teardown():
     print "package teardown"
     shutil.rmtree("temp_output", ignore_errors=True)
