5ffdafd7f474e4ac2ad27ca6d39c7ed0a0d967ca
uweschmitt <uschmitt@mineway.de>
2011-08-24 15:48:32 +0200

    added unittest for nativeID support for specs

--- unittests/testMSExperiment.py
+++ unittests/testMSExperiment.py
@@ -1,6 +1,7 @@
 
 import sys
 sys.path.insert(0, "..")
+
 from pyOpenMS import *
 
 def test_loadMzXMLFile():
@@ -11,6 +12,7 @@ def test_loadMzXMLFile():
     assert len(msExp) == 41 # 2884 # 2005
     
     spec0 = msExp.specs[0]
+    assert spec0.id=="scan=1"
     assert spec0.polarization == "+"
     assert spec0.precursors == []
     assert spec0.msLevel == 1
@@ -18,6 +20,7 @@ def test_loadMzXMLFile():
 
 
     spec0 = msExp.specs[40]
+    assert spec0.id=="scan=41"
     assert spec0.polarization == "+"
     assert len(spec0.precursors) == 1
     assert len(spec0.precursors[0]) == 2
@@ -25,3 +28,26 @@ def test_loadMzXMLFile():
     assert len(spec0) == 121 
 
 
+def test_loadMzMLFile():
+    saveMzMlFile(loadMzXmlFile("data/SHORT_MS2_FILE.mzXML"), "temp_output/short.mzML")
+    msExp = loadMzMlFile("temp_output/short.mzML")
+
+    assert msExp != None
+
+    assert len(msExp) == 41 # 2884 # 2005
+    
+    spec0 = msExp.specs[0]
+    assert spec0.id=="scan=1"
+    assert spec0.polarization == "+"
+    assert spec0.precursors == []
+    assert spec0.msLevel == 1
+    assert len(spec0) == 21
+
+
+    spec0 = msExp.specs[40]
+    assert spec0.id=="scan=41"
+    assert spec0.polarization == "+"
+    assert len(spec0.precursors) == 1
+    assert len(spec0.precursors[0]) == 2
+    assert spec0.msLevel == 2
+    assert len(spec0) == 121 
