8b9453b76c3e22cd4f6d86877d813d25b442c751
uweschmitt <uschmitt@mineway.de>
2011-09-09 14:18:11 +0200

    added batch for matched filters

--- batches/feature_detector.py
+++ batches/feature_detector.py
@@ -87,3 +87,91 @@ def runCentwave(pattern=None, destination=None, configid=None, **params):
     return P(configs.centwaveConfig, True).run(pattern, destination, configid, **params)
             
 
+def runMatchedFilter(pattern=None, destination=None, configid=None, **params):
+
+    """
+         runs matched filters algorithm from xcms in batch mode.
+         input files are map files (mzXML, mxML, mzData),
+         ouput files are csv files
+
+         you can add modifications to the standard pamaeters, eg ppm,
+         as named arguments.
+
+         if you have multiple configs for centwave, you can give an
+         configid as defined in configs.py, or you are asked to choose
+         a config.
+
+         if you have a single config this one is used automatically
+
+         examples:
+                
+              runMatchedFilter():
+                     asks for source files and target directory
+                     asks for config if multiple configs are defined
+
+              runMatchedFilter(configid="std", ppm=17)
+                     uses config with id "std", overwrites ppm parameter
+                     with ppm=17.
+
+              runMatchedFilter(ppm=13):
+                     asks for source files and target directory
+                     runs centwave with modified ppm=13 parameter.
+                     
+              runMatchedFilter(pattern):
+                     looks for map files matching pattern
+                     resulting csv files are stored next to input map file
+
+              runMatchedFilter(pattern, msDiff=0.003):
+                     looks for map files matching pattern
+                     resulting csv files are stored next to input map file
+                     runs centwave with modified msDiff parameter
+
+              runMatchedFilter(pattern, destination):
+                     looks for map files matching pattern
+                     resulting csv files are stored at destination directory
+                    
+              runMatchedFilter(pattern, destination, ppm=17, peakwidth=(5,100) ):
+                     looks for map files matching pattern
+                     resulting csv files are stored at destination directory
+                     runs centwave with modified ppm and peakwidth parameters.
+
+    """
+
+    from BatchRunner import BatchRunner
+    import configs
+    import libms.RConnect
+    import ms
+    import os.path
+
+    class P(BatchRunner):
+
+        def setup(self, config):
+            self.det = libms.RConnect.MatchedFilterFeatureDetector(**config)
+
+        def process(self, path):
+            
+
+            try:
+                print "read ", path
+                ds = ms.loadMap(path)
+            except Exception, e:
+                print e
+                print "reading FAILED"
+                return None
+            
+            table = self.det.process(ds)
+            table.title = path
+
+            print len(table), "features found"
+            return table
+
+        def write(self, result, destinationDir, path):
+            basename, ext = os.path.splitext(os.path.basename(path))
+            savePath = os.path.join(destinationDir, basename+".csv")
+            print "save to ", savePath
+            result.saveCSV(savePath)
+
+
+    return P(configs.matchedFilterConfig, True).run(pattern, destination, configid, **params)
+            
+
--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 #encoding: utf-8
 
-from libms.RConnect.XCMSConnector import CentwaveFeatureDetector
+from libms.RConnect.XCMSConnector import CentwaveFeatureDetector, MatchedFilterFeatureDetector
 
 def union(d1, d2):
     d = d1.copy()
@@ -25,6 +25,13 @@ centwaveConfigLong= dict( ppm=5,
 centwaveConfig = [   ("std", "standard config orbitrap", union(cwfstd, centwaveConfigStd)) ,
                      ("long", "long retention config"  , union(cwfstd, centwaveConfigLong)) ,
                  ]
+
+
+mfstd = MatchedFilterFeatureDetector.standardConfig
+
+matchedFilterStd = dict ( )
+
+matchedFilterConfig = [  ( "std", "standard config" , mfstd ) ]
 
 
 pathToProteoWizard = r"C:\Dokumente und Einstellungen\Administrator\Eigene Dateien\proteowizard"
