fcf67a8babf74418b81c1806245f120b69bba724
uweschmitt <uschmitt@mineway.de>
2011-09-09 13:47:59 +0200

    added mz-limits selection to mzExplorer

--- libms/Explorers/mzExplorer.py
+++ libms/Explorers/mzExplorer.py
@@ -1,69 +1,149 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4.QtGui import  QVBoxLayout, QDialog, QPainter, QMainWindow
-from PyQt4.QtCore import Qt
+from PyQt4.QtGui import  QVBoxLayout, QDialog, QPainter, QMainWindow, QLabel, QLineEdit, QPushButton, QHBoxLayout
+from PyQt4.QtCore import Qt, SIGNAL
 
-#from guiqwt.plot import CurveWidget, PlotManager
-#from guiqwt.builder import make
-#from guiqwt.label import ObjectInfo
 import guidata
 import sys
 
-#+from ModifiedGuiQwtBehavior import *
-#from Config import *
 
 import guiqwt
 assert guiqwt.__version__ == "2.1.4"
 
-#sys.path.insert(0, "..")
-
-
 
 from PlottingWidgets import RtPlotter, MzPlotter
-
 import numpy as np
-
-
-
 
 class MzExplorer(QDialog):
 
     def __init__(self):
         QDialog.__init__(self)
-        
-        #self.manager = PlotManager(self)
         self.setWindowFlags(Qt.Window)
-
 
     def setup(self, peakmap):
 
         self.processPeakmap(peakmap)
         self.setupPlotWidgets()
+        self.setupInputWidgets()
+        self.connectSignalsAndSlots()
         self.setupLayout()
 
+        self.resetMzLimits()
         self.plotChromatogramm()
         self.rtPlotter.notifyMZ()
 
-        self.rtPlotter.setXAxisLimits(10, 20)
-        
     def closeEvent(self, evt):
         pass
 
     def processPeakmap(self, peakmap):
         self.peakmap = peakmap
         self.rts = np.array([s.RT for s in self.peakmap])
-        self.chromatogram = np.array([np.sum(spec.peaks[:, 1]) for spec in peakmap])
+
+        mzvals = np.hstack([ spec.peaks[:,0] for spec in peakmap ])
+        self.absMinMZ = np.min(mzvals)
+        self.absMaxMZ = np.max(mzvals)
+        self.minMZ = self.absMinMZ
+        self.maxMZ = self.absMaxMZ
+        self.updateChromatogram()
+
+
+    def updateChromatogram(self):
+        min_, max_ = self.minMZ, self.maxMZ
+        cc =[np.sum(spec.peaks[(spec.peaks[:,0] >= min_) * (spec.peaks[:,0] <= max_)][:, 1]) for spec in self.peakmap]
+        self.chromatogram = np.array(cc)
+
+
+    def connectSignalsAndSlots(self):
+
+        self.connect(self.selectButton, SIGNAL("clicked()"), self.selectButtonPressed)
+        self.connect(self.resetButton, SIGNAL("clicked()"), self.resetButtonPressed)
+
+    def selectButtonPressed(self):
+        try:
+            self.minMZ=float(self.inputMin.text())
+            self.maxMZ=float(self.inputMax.text())
+            self.mzPlotter.setXAxisLimits(self.minMZ, self.maxMZ)
+            self.updateChromatogram()
+            self.plotChromatogramm()
+        except Exception, e:
+            print e
+
+    def resetButtonPressed(self):
+        self.resetMzLimits()
+
+    def resetMzLimits(self):
+        self.minMZ = self.absMinMZ
+        self.maxMZ = self.absMaxMZ
+        self.mzPlotter.setXAxisLimits(self.minMZ, self.maxMZ)
+        self.inputMin.setText("%.6f" % self.absMinMZ)
+        self.inputMax.setText("%.6f" % self.absMaxMZ)
+
+        self.updateChromatogram()
+        self.plotChromatogramm()
+    
+        
 
     def setupLayout(self):
         vlayout = QVBoxLayout()
         self.setLayout(vlayout)
+
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(self.labelMin)
+        hlayout.addWidget(self.inputMin)
+        hlayout.addWidget(self.labelMax)
+        hlayout.addWidget(self.inputMax)
+        hlayout.addWidget(self.selectButton)
+        hlayout.addWidget(self.resetButton)
+
+        vlayout.addLayout(hlayout)
+
         vlayout.addWidget(self.rtPlotter.widget)
         vlayout.addWidget(self.mzPlotter.widget)
+
+    def setupInputWidgets(self):
+        self.labelMin = QLabel("minrz")
+        self.labelMax = QLabel("maxrz")
+        self.inputMin = QLineEdit()
+        self.inputMax = QLineEdit()
+        self.selectButton = QPushButton()
+        self.selectButton.setText("Select")
+        self.resetButton = QPushButton()
+        self.resetButton.setText("Reset")
+
+        self.inputMin.setText("%.6f" % self.absMinMZ)
+        self.inputMax.setText("%.6f" % self.absMaxMZ)
+
+
+
+        """
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+
+        self.label_2 = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.horizontalLayout.addWidget(self.label_2)
+        self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout.addWidget(self.label)
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.lineEdit_2 = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.horizontalLayout.addWidget(self.lineEdit_2)
+        self.lineEdit = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        """
+
 
 
     def setupPlotWidgets(self):
         self.rtPlotter = RtPlotter(self.rts , self.plotMz)
         self.mzPlotter = MzPlotter(self.peakmap)
+
 
         self.rtPlotter.setMinimumSize(600, 300)
         self.mzPlotter.setMinimumSize(600, 300)
