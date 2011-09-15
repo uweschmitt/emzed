2fdfe162c4726b319b9682d38aefbc45010c9f3d
uweschmitt <uschmitt@mineway.de>
2011-09-15 12:16:43 +0200

    added file missing in last commit

--- configs.py
+++ configs.py
@@ -43,6 +43,6 @@ PeakIntegrators = dict( std     = SGIntegrator(window_size=11, order=2),
                         sg_21_2 = SGIntegrator(window_size=21, order=2),
                         sg_11_1 = SGIntegrator(window_size=11, order=1),
                         sg_21_1 = SGIntegrator(window_size=21, order=1),
-                        asym_gauss = AssymetricGaussIntegrator(),
+                        asym_gauss = AsymmetricGaussIntegrator(),
                       )
 
