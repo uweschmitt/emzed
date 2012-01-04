4d5555c75d6f4839569fc1c45217baf1108c6996
uweschmitt <uschmitt@mineway.de>
2012-01-04 18:52:07 +0100

    cleanup symbols in libms/Statistics/Anova + extended tests

--- libms/Statistics/Anova.py
+++ libms/Statistics/Anova.py
@@ -1,24 +1,24 @@
-from collections import defaultdict
-from scipy.stats import f_oneway, kruskal
-import numpy
+from collections import defaultdict as  _defaultdict
+from scipy.stats import f_oneway as _f_oneway, kruskal as _kruskal
+import numpy as _numpy
 
 def _getSamples(factorColumn, dependentColumn, minsize=1):
     factors, _ = factorColumn._eval(None)
     dependents, _ = dependentColumn._eval(None)
-    groups = defaultdict(list)
+    groups = _defaultdict(list)
     for factor, depenent in zip(factors, dependents):
         groups[factor].append(depenent)
     samples = groups.values()
     if any(len(s) < minsize for s in samples):
         print "WARNING: sample has less than %d subjects" % minsize
-    return map(numpy.array, samples)
+    return map(_numpy.array, samples)
 
 def oneWayAnova(factorColumn, dependentColumn):
-    F, p = f_oneway(*_getSamples(factorColumn, dependentColumn))
+    F, p = _f_oneway(*_getSamples(factorColumn, dependentColumn))
     return F, p
 
 def kruskalWallis(factorColumn, dependentColumn):
-    H, p = kruskal(*_getSamples(factorColumn, dependentColumn, 5))
+    H, p = _kruskal(*_getSamples(factorColumn, dependentColumn, 5))
     return H, p
 
 
--- unittests/testAnova.py
+++ unittests/testAnova.py
@@ -1,13 +1,13 @@
 import ms
 
-def testOneWay():
+def testOnColumns():
     t = ms.toTable("factor", [1,2,1,2,1,1,2])
     t.addColumn("dependent", t.factor*1.1)
     F, p = ms.oneWayAnova(t.factor, t.factor*1.1)
-    assert p<1e-7, p
+    assert p<1e-12, p
 
     H, p = ms.kruskalWallis(t.factor, t.factor*1.1)
-    print H, p
+    assert abs(p-0.014305)/0.014305 < 1e-4
 
 
     t.addColumn("dependent2", [1.01,2.01,1.02,2.02,.99, 0.98,1.98])
@@ -15,4 +15,8 @@ def testOneWay():
     assert abs(p-1.3e-8)/1.3e-8 < 0.01
 
     H, p = ms.kruskalWallis(t.factor, t.dependent2)
-    print H, p
+    assert abs(p-0.033894)/0.033894 < 1e-4
+
+def testOnTables():
+    pass
+
