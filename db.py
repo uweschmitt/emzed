2443a6e7fa58d53f1d2329bb8315982e2ee110a0
uweschmitt <uschmitt@mineway.de>
2012-01-26 16:53:22 +0100

    init website

--- db.py
+++ db.py
@@ -6,41 +6,43 @@ import os
 exchangeFolder = userConfig.getExchangeFolder()
 dbPath = os.path.join(exchangeFolder, "pubchem.db")
 pubChemDB = PubChemDB(dbPath)
-newIds, missingIds = pubChemDB.synchronize()
-if len(newIds) and not len(missingIds):
-    print "PUBCHEM NOT UP TO DATE"
-    if os.access(exchangeFolder, os.W_OK):
-        pubChemDB.update(newIds)
-        pubChemDB.store()
-    else:
-        print
-        print "*"*65
-        print "YOU ARE NOT ALLOW TO UPDATE GLOBAL DB"
-        print "PLEASE INFORM YOUR ADMINISTRATOR"
-        print "*"*65
-        print
-elif len(missingIds):
-    print "PUBCHEM OUT OF SYNC"
-    if os.access(exchangeFolder, os.W_OK):
-        print
-        print "PLEASE RUN db.pubchem.reset() AND RESTART SHELL TO"
-        print "GET A NEW VERSION OF pubChemDB"
-        print
-        #pubChemDB.reset(100)
 
-    else:
-        print
-        print "*"*65
-        print "YOU ARE NOT ALLOW TO UPDATE GLOBAL DB"
-        print "PLEASE INFORM YOUR ADMINISTRATOR"
-        print "*"*65
-        print
+if not os.environ.get("NO_PUBCHEM_UPDATE"):
+    newIds, missingIds = pubChemDB.synchronize()
+    if len(newIds) and not len(missingIds):
+        print "PUBCHEM NOT UP TO DATE"
+        if os.access(exchangeFolder, os.W_OK):
+            pubChemDB.update(newIds)
+            pubChemDB.store()
+        else:
+            print
+            print "*"*65
+            print "YOU ARE NOT ALLOW TO UPDATE GLOBAL DB"
+            print "PLEASE INFORM YOUR ADMINISTRATOR"
+            print "*"*65
+            print
+    elif len(missingIds):
+        print "PUBCHEM OUT OF SYNC"
+        if os.access(exchangeFolder, os.W_OK):
+            print
+            print "PLEASE RUN db.pubchem.reset() AND RESTART SHELL TO"
+            print "GET A NEW VERSION OF pubChemDB"
+            print
+            #pubChemDB.reset(100)
+
+        else:
+            print
+            print "*"*65
+            print "YOU ARE NOT ALLOW TO UPDATE GLOBAL DB"
+            print "PLEASE INFORM YOUR ADMINISTRATOR"
+            print "*"*65
+            print
+    del newIds, missingIds
+
 
 # cleanup namespace
 del os
 del dbPath
-del newIds
-del missingIds
-del exchangeFolder
 del userConfig
+del exchangeFolder
 del PubChemDB
---
+++ website_source/_build/html/_sources/api.txt
@@ -0,0 +1,6 @@
+API Overview
+============
+
+.. automodule:: libms.DataStructures.Table
+
+.. automodule:: libms.DataStructures.Expressions
