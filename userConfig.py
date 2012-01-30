744d4650037b7f5954d2bd3a750b1871e7b325b7
uweschmitt <uschmitt@mineway.de>
2012-01-30 20:19:05 +0100

    fixed error in userConfig in case of abort dialog + improvmenet for no exchange folder

--- userConfig.py
+++ userConfig.py
@@ -2,7 +2,7 @@ import _winreg
 import os
 import ConfigParser
 
-class GlobalConfig(object):
+class _GlobalConfig(object):
 
     def __init__(self):
         key =_winreg.OpenKey(_winreg.HKEY_CURRENT_USER,
@@ -31,23 +31,34 @@ class GlobalConfig(object):
         app = guidata.qapplication()
         import guidata.dataset.datatypes as dt
         import guidata.dataset.dataitems as di
+
+        def check_value(self, value):
+            if not isinstance(value, self.type):
+                return False
+            if str(value).strip()=="":
+                return True
+            return di.DirectoryItem._check_value(self, value)
+
+        di.DirectoryItem._check_value = di.DirectoryItem.check_value
+        di.DirectoryItem.check_value = check_value
         class ConfigEditor(dt.DataSet):
             """ ConfigEditor
 
                 Please provide a global exchange folder for databases,
                 scripts and configs shared among your lab.
 
-                If you work only locally, you can abort this dialog.
+                If you do not have such an exchange folder, just click 'Ok'.
             """
-            exchangeFolder = di.DirectoryItem("Global exchange folder:")
+            exchangeFolder = di.DirectoryItem("Global exchange folder:", notempty=False)
 
         dlg = ConfigEditor()
         dlg.edit()
         self.exchangeFolder = dlg.exchangeFolder
+        di.DirectoryItem.check_value = di.DirectoryItem._check_value
 
     def saveConfig(self):
         p = ConfigParser.ConfigParser()
-        p.set("DEFAULT", "exchange_folder", self.exchangeFolder)
+        p.set("DEFAULT", "exchange_folder", self.exchangeFolder or "")
         p.write(open(self.configFilePath, "w"))
 
 
@@ -66,9 +77,10 @@ def getDataHome():
     return dataHome
 
 def getExchangeFolder():
-    folder = GlobalConfig().getExchangeFolder()
-    if folder is not None:
+    folder = _GlobalConfig().getExchangeFolder()
+    if folder:
         return folder
+    # no global exchange folder set, use local folder instead:
     exchangeFolder = os.path.join(getDataHome(), "shared")
     if not os.path.exists(exchangeFolder):
         os.makedirs(exchangeFolder)
