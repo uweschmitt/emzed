283555b5ac2a5315fb7644796cea491085a41d2c
uweschmitt <uschmitt@mineway.de>
2012-02-10 12:51:54 +0100

    added README to installer

--- installer/build_installer.py
+++ installer/build_installer.py
@@ -59,7 +59,7 @@ def buildZipFile(zip_name, files, exclude=[], relocate_path=".", prefixpath=".")
 # todo: relocate_path mit files verwurschteldn ?
 buildZipFile("emzed_files.zip", files, exclude = [".*", "*.pyc"], relocate_path="..")
 
-buildZipFile("emzed.zip", ["installer.bat", "installer.py", "License.txt", "emzed_files.zip"], prefixpath="emzed_1.0")
+buildZipFile("emzed.zip", ["README", "installer.bat", "installer.py", "License.txt", "emzed_files.zip"], prefixpath="emzed_1.0")
 
 import os
 os.remove("emzed_files.zip")
