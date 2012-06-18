e74435f02f59683d735787f6e62a6d59aa4c74be
Uwe Schmitt <uschmitt@mineay.de>
2012-06-18 03:17:09 -0700

    modified folder init at startup procedure

--- startup/850initilizeFolders.py
+++ startup/850initilizeFolders.py
@@ -4,16 +4,19 @@ _dataHome = _userConfig.getDataHome()
 
 if not os.path.exists(_dataHome):
     os.makedirs(_dataHome)
-    _examples = os.path.join(_dataHome, "example_scripts")
+    
+_flag_file = os.path.join(_dataHome, ".first_init_done")
+
+_first_start = not os.path.exists(_flag_file)
+
+if _first_start:
+    open(_flag_file, "w").close()
+    
+    examples = os.path.join(_dataHome, "example_scripts")
     if not os.path.exists(_examples):
-        shutil.copytree("example_scripts", _examples)
-    for _p in glob.glob("example_scripts/*.mzXML"):
-        shutil.copy(_p, _dataHome)
-    for _p in glob.glob("example_scripts/*.csv"):
-        shutil.copy(_p, _dataHome)
-
-    os.chdir(_dataHome) # first start
-
-
-
-
+	shutil.copytree("example_scripts", _examples)
+	for _p in glob.glob("example_scripts/*.mzXML"):
+	    shutil.copy(_p, _dataHome)
+	for _p in glob.glob("example_scripts/*.csv"):
+	    shutil.copy(_p, _dataHome)
+    os.chdir(_dataHome)
