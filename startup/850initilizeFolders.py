51d46223fe9b8e4e9db7b756acb033529e239873
Uwe Schmitt <uschmitt@mineay.de>
2012-06-19 02:44:09 -0700

    mod of startup procedures for local emzed_files

--- startup/850initilizeFolders.py
+++ startup/850initilizeFolders.py
@@ -10,13 +10,8 @@ _flag_file = os.path.join(_dataHome, ".first_init_done")
 _first_start = not os.path.exists(_flag_file)
 
 if _first_start:
+    # set flag
     open(_flag_file, "w").close()
-    
-    examples = os.path.join(_dataHome, "example_scripts")
-    if not os.path.exists(_examples):
-	shutil.copytree("example_scripts", _examples)
-	for _p in glob.glob("example_scripts/*.mzXML"):
-	    shutil.copy(_p, _dataHome)
-	for _p in glob.glob("example_scripts/*.csv"):
-	    shutil.copy(_p, _dataHome)
+
+    shutil.copytree("emzed_files", _dataHome)
     os.chdir(_dataHome)
