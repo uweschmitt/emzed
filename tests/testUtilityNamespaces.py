b2831ae60713dd55239794d201aa1481de109f8e
Uwe Schmitt <uschmitt@mineway.de>
2012-08-30 14:24:20 +0200

    implemented first version of adducts module, incl. docs and tests

--- tests/testUtilityNamespaces.py
+++ tests/testUtilityNamespaces.py
@@ -1,6 +1,7 @@
 import elements
 import mass
 import abundance
+import adducts
 
 def testAccessAndConsistency():
     c12 = elements.C12
@@ -12,6 +13,50 @@ def testAccessAndConsistency():
     assert c12["number"] == 6
     assert  mass.of("[13]C") - mass.of("C") == mass.C13-mass.C12
 
+
+def testAdducts():
+    assert len(adducts.all) == 21, len(adducts.all)
+    #test subgroups
+    assert len(adducts.positive) == 14, len(adducts.positive)
+    assert len(adducts.negative) == 21-14, len(adducts.negative)
+    assert len(adducts.single_charged)>0
+    assert len(adducts.double_charged)>0
+    assert len(adducts.triple_charged )>0
+    assert len(adducts.positive_single_charged)>0
+    assert len(adducts.positive_double_charged)>0
+    assert len(adducts.positive_triple_charged)>0
+    assert len(adducts.negative_single_charged)>0
+    assert len(adducts.negative_double_charged)>0
+    assert len(adducts.negative_triple_charged )>0
+
+
+    # tst namespace constants
+    assert adducts.M_plus_H[-1] == +1
+    assert adducts.M_plus_NH4[-1] == +1
+    assert adducts.M_plus_Na[-1] == 1
+    assert adducts.M_plus_H_minus_H2O[-1]  ==1
+    assert adducts.M_plus_H_minus_H4O2[-1]  ==1
+    assert adducts.M_plus_K[-1]  ==1
+    assert adducts.M_plus_CH4O_plus_H[-1]  ==1
+    assert adducts.M_plus_2Na_minus_H[-1]  ==1
+    assert adducts.M_plus_H2[-1]  ==2
+    assert adducts.M_plus_H3[-1]  ==3
+    assert adducts.M_plus_Na_plus_H[-1]  ==2
+    assert adducts.M_plus_H2_plus_Na[-1]  ==3
+    assert adducts.M_plus_Na2[-1]  ==2
+    assert adducts.M_plus_H_plus_Na2[-1]  ==3
+    assert adducts.M_minus_H[-1]  ==-1
+    assert adducts.M_minus_H_minus_H2O[-1] == -1
+    assert adducts.M_plus_Na_minus_H2[-1]  ==-1
+    assert adducts.M_plus_Cl[-1]  ==-1
+    assert adducts.M_plus_K_minus_H2[-1]  ==-1
+    assert adducts.M_minus_H2[-1]  ==-2
+    assert adducts.M_minus_H3[-1]  ==-3
+    assert adducts.M_plus_Na_minus_H2[-1] == -1
+
+    t = adducts.positive.toTable()
+    assert len(t) == 14
+    assert len(t.colNames) == 3
 
 
 
--- website/api_other.rst
+++ website/api_other.rst
@@ -9,6 +9,7 @@ API helper modules
    import elements
    import mass
    import abundance
+   import adducts
 
 Data of chemical elemets are available from the *elements* module, e.g:
 
@@ -49,3 +50,56 @@ And isotopes can be specified in brackets:
    print abundance.C
    print abundance.C[12]
    print abundance.C12
+
+
+.. automodule:: adducts
+
+The *adducts* module provides information about most common ESI adducts,
+adduct lists can be converted to a *Table* eg:
+
+.. pycon::
+
+    print adducts.labels
+    subgroup = adducts.get("[M+H]+", "[M+Na]+")
+    subgroup.toTable().print_()
+
+    print adducts.namedLabels
+    print adducts.M_plus_H
+    print adducts.M_plus_Na_minus_H2
+
+
+The following preselected groups of adducts exist:
+
+.. pycon::
+
+    print len(adducts.all)
+    print len(adducts.positive)
+    print len(adducts.negative)
+    print len(adducts.single_charged)
+    print len(adducts.double_charged)
+    print len(adducts.triple_charged)
+    print len(adducts.positive_single_charged)
+    print len(adducts.positive_double_charged)
+    print len(adducts.positive_triple_charged)
+    print len(adducts.negative_single_charged)
+    print len(adducts.negative_double_charged)
+    print len(adducts.negative_triple_charged)
+
+
+Further a default dialog can be opened for asking a multiple choice
+selection of all adducts or of a subgroup:
+
+.. image:: adduct_dialog.png
+
+.. pycon::
+
+    tab = adducts.all.askForSelection().toTable() !noexec
+    tab.print_() !noexec
+    adduct_name mass_shift z       !asoutput
+    str         float      int     !asoutput
+    ------      ------     ------  !asoutput
+    [M+H]+      1.007276   +1      !asoutput
+    [M+2Na-H]+  44.971165  +1      !asoutput
+    [M-H]-      -1.007276  -1      !asoutput
+    [M-H-H2O]-  -19.017842 -1      !asoutput
+
