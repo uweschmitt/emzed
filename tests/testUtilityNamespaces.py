28cde9e1e5e14e280dde2ccc50439d9434110e87
Uwe Schmitt <uschmitt@mineway.de>
2012-10-01 13:36:53 +0200

    simplified test for commit 3a1b1060

--- tests/testUtilityNamespaces.py
+++ tests/testUtilityNamespaces.py
@@ -12,8 +12,8 @@ def testAccessAndConsistency():
     assert c12["name"] == "Carbon"
     assert c12["number"] == 6
     assert  mass.of("[13]C") - mass.of("C") == mass.C13-mass.C12
-    assert  mass.of("C", C=mass.C13) - mass.of("C") == mass.C13-mass.C12
-    assert  mass.of("C", C=elements.C13) - mass.of("C") == mass.C13-mass.C12
+    assert  mass.of("C", C=mass.C13) == mass.of("[13]C")
+    assert  mass.of("C", C=elements.C13) == mass.of("[13]C")
 
 
 def testAdducts():
