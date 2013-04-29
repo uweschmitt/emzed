uweschmitt/emzed

commit f5f911502e5c77be50d027f2dfc40eff2a24efd6

[TEST] compressing peakmaps in tables is testable now

libms/DataStructures/Table.py
@@ -1450,3 +1450,41 @@ def toOpenMSFeatureMap(table):
         f.setIntensity(area if area is not None else 1000.0)
         fm.push_back(f)
     return fm
+
+
+def compressPeakMaps(table):
+    """
+    sometimes duplicate peakmaps occur as different objects in a table,
+    that is: different id() but same content.
+    this function removes duplicates and replaces different instances
+    of the same data by one particular instance.
+    """
+    import hashlib
+
+    def _compute_digest(pm):
+        h = hashlib.sha512()
+        for spec in pm.spectra:
+            # peaks.data is binary representation of numpy array peaks:
+            h.update(str(spec.peaks.data))
+        return h.digest()
+
+    from   libms.DataStructures import PeakMap
+    peak_maps = dict()
+    digests = dict()
+    for row in table.rows:
+        for cell in row:
+            if isinstance(cell, PeakMap):
+                if not hasattr(cell, "_digest"):
+                    d = digests.get(id(cell))
+                    if d is None:
+                        d = _compute_digest(cell)
+                        digests[id(cell)] = d
+                    cell._digest = d
+                peak_maps[cell._digest] = cell
+    for row in table.rows:
+        for i, cell in enumerate(row):
+            if isinstance(cell, PeakMap):
+                row[i] = peak_maps[cell._digest]
+                del cell._digest
+    table.resetInternals()
+

ms/load_utils.py
@@ -1,8 +1,5 @@
 #encoding: utf-8
 
-
-
-
 def loadPeakMap(path=None):
     """ loads mzXML, mzML and mzData files
 
@@ -40,7 +37,6 @@ def loadPeakMap(path=None):
     return PeakMap.fromMSExperiment(experiment)
 
 
-
 def loadTable(path=None):
     """ load pickled table
 
@@ -51,8 +47,7 @@ def loadTable(path=None):
     # local import in order to keep namespaces clean
     import ms
     import sys
-    import tools
-    from   libms.DataStructures.Table import Table
+    from   libms.DataStructures.Table import Table, compressPeakMaps
 
     if isinstance(path, unicode):
         path = path.encode(sys.getfilesystemencoding())
@@ -62,7 +57,7 @@ def loadTable(path=None):
             return None
 
     result = Table.load(path)
-    tools.compressPeakMaps(result)
+    compressPeakMaps(result)
     return result
 
 def loadCSV(path=None, sep=";", keepNone = False, **specialFormats):

ms/store_utils.py
@@ -40,9 +40,9 @@ def storeTable(tab, path=None, forceOverwrite=False):
     # local import in order to keep namespaces clean
     import ms
     import sys
-    import tools
+    from libms.DataStructures.Table import compressPeakMaps
 
-    tools.compressPeakMaps(tab)
+    compressPeakMaps(tab)
 
     if isinstance(path, unicode):
         path = path.encode(sys.getfilesystemencoding())

ms/tools.py
@@ -198,32 +198,3 @@ def recalculateMzPeaks(table):
                 table.addColumn(mz_col, _recalculateMzPeakFor(postfix),
                                 format="%.5f", type_=float)
 
-
-def compressPeakMaps(table):
-    import hashlib
-
-    def _compute_digest(pm):
-        h = hashlib.sha512()
-        for spec in pm.spectra:
-            h.update(str(spec.peaks.data))
-        return h.digest()
-
-    from   libms.DataStructures import PeakMap
-    peak_maps = dict()
-    digests = dict()
-    for row in table.rows:
-        for cell in row:
-            if isinstance(cell, PeakMap):
-                if not hasattr(cell, "_digest"):
-                    d = digests.get(id(cell))
-                    if d is None:
-                        d = _compute_digest(cell)
-                        digests[id(cell)] = d
-                    cell._digest = d
-                peak_maps[cell._digest] = cell
-    for row in table.rows:
-        for i, cell in enumerate(row):
-            if isinstance(cell, PeakMap):
-                row[i] = peak_maps[cell._digest]
-    table.resetInternals()
-

tests/testTable.py
@@ -526,3 +526,24 @@ def testApply():
     ts = t.splitBy("a_bin")
     assert len(ts) == 4
 
+
+def testCompress():
+    t = ms.toTable("a", [])
+    from libms.DataStructures.Table import compressPeakMaps
+    from libms.DataStructures.MSTypes import PeakMap, Spectrum
+    import numpy
+    compressPeakMaps(t)
+
+    s = Spectrum(numpy.arange(12).reshape(-1,2), 1.0, 1, "+")
+    pm = PeakMap([s])
+    s = Spectrum(numpy.arange(12).reshape(-1,2), 1.0, 1, "+")
+    pm2 = PeakMap([s])
+
+    t = ms.toTable("pm", [pm, pm2])
+    assert len(set(map(id, t.pm.values))) == 2
+    compressPeakMaps(t)
+    assert len(set(map(id, t.pm.values))) == 1
+
+
+
+

commit 3e3edc0a8cc4f4e51f909a5c2aae4a1208a0ef27

[FIX] fixed regression from commit f5f911502e5c77be50d027f2dfc40eff2a24efd6

libms/DataStructures/Table.py
@@ -1485,6 +1485,6 @@ def compressPeakMaps(table):
         for i, cell in enumerate(row):
             if isinstance(cell, PeakMap):
                 row[i] = peak_maps[cell._digest]
-                del cell._digest
+                # del cell._digest
     table.resetInternals()
 