d9697af10d01d57579852574fc474a35a9fdc811
Uwe Schmitt <uschmitt@mineway.de>
2013-05-07 13:29:32 +0200

    [FEATURE] adapted win installer to winpython distribution

---
+++ installer_files/run_installer.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 07 11:55:43 2013
+
+@author: uweschmi
+"""
+
+
+import os.path
+import _winreg
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+def getPathToPythonExe():
+    try:
+        k0 = _winreg.OpenKey(_winreg.HKEY_CLASSES_ROOT,
+                             r"Python.File\Shell\open\command")
+    except WindowsError:
+        try:
+            k0 = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,
+                                r"Software\Classes\Python.File\Shell\open\command")
+        except WindowsError:
+            return None
+
+    value = _winreg.QueryValue(k0, "").split(" ")[0].strip("'").strip('"')
+    path = os.path.dirname(value)
+    return path
+
+path_to_python_exe = getPathToPythonExe()
+print "path to python.exe is", path_to_python_exe
+if path_to_python_exe is not None:
+    os.chdir(path_to_python_exe)
+os.system("python %s %s" % (os.path.join(here, "_installer.py"), here))
